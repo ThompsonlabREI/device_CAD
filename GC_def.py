45abfa53ddd85573727fa887af59d4b92f291e77
Sharon Ruth Platt <srsplatt@dynamic-oit-vapornet100-10-9-53-222.princeton.edu>
2022-12-07 12:56:05 -0500

    boolean worked as expected and outline calculated

--- GC_def.py
+++ GC_def.py
@@ -7,50 +7,39 @@ import phidl.path as pp
 
 def generate_grating_holes(air_hole_diameter_list_base,
     constgrating_airhole_scale_factor,
-    grating_period_x_start,
-    phase_factor,
-    grating_delta_index,
-    resonance_freq,
-    num_grating_periods_x,
-    num_grating_periods_y,
-    num_points,
-    a_2DPhC_nm,
-    grating_pad_length,
-    grating_pad_width,
-    grating_pad_buffer,
-    grating_pad_center_x,
-    grating_pad_center_y,
-    layer_num):
+    GCparams,
+    grating_pad_origin,
+    GC_hole_layer):
     SWG_holes = Device()
 
     air_hole_diameter_list = numpy.asarray(
         air_hole_diameter_list_base) * constgrating_airhole_scale_factor  # SRP: varies from 0.85 to 1.05 for first run
     # air_hole_diameter_list = numpy.asarray(air_hole_diameter_list_base) + constgrating_airholescale_list[iB] # take off the 1.13 scaling factor so that GC hole sizes match lumerical
-    grating_start_x = grating_pad_center_x - grating_pad_length / 2.0 + grating_pad_buffer
-    grating_start_y = grating_pad_center_y - grating_pad_width / 2.0 + grating_pad_buffer
-    grating_periods_x_list = numpy.zeros(len(air_hole_diameter_list)) + grating_period_x_start
+    grating_start_x = grating_pad_origin[0] - GCparams['grating_pad_length'] / 2.0 + GCparams['grating_pad_buffer']
+    grating_start_y = grating_pad_origin[1] - GCparams['grating_pad_width'] / 2.0 + GCparams['grating_pad_buffer']
+    grating_periods_x_list = numpy.zeros(len(air_hole_diameter_list)) + GCparams['grating_period_x_start']
     for iG in range(1, len(air_hole_diameter_list), 1):
-        grating_periods_x_list[iG] = grating_periods_x_list[iG - 1] + phase_factor * grating_delta_index * \
-                                     grating_periods_x_list[iG - 1] * grating_periods_x_list[iG - 1] / resonance_freq
+        grating_periods_x_list[iG] = grating_periods_x_list[iG - 1] + GCparams['phaseFactor'] * GCparams['grating_delta_index'] * \
+                                     grating_periods_x_list[iG - 1] * grating_periods_x_list[iG - 1] / GCparams['resonance']
 
-    for iGx in range(num_grating_periods_x):
-        for iGy in range(num_grating_periods_y):
+    for iGx in range(GCparams['num_grating_periods_x']):
+        for iGy in range(GCparams['num_grating_periods_y']):
 
             grating_column_start_x = grating_start_x + air_hole_diameter_list[iGx] / 2.0 + numpy.sum(
                 grating_periods_x_list[:iGx])
-            grating_column_start_y = grating_start_y + numpy.sqrt(3) * a_2DPhC_nm * iGy
-            hole = pg.circle(radius=air_hole_diameter_list[iGx] / 2.0, angle_resolution=2.5, layer=layer_num)
+            grating_column_start_y = grating_start_y + numpy.sqrt(3) * GCparams['a_2DPhC'] * iGy
+            hole = pg.circle(radius=air_hole_diameter_list[iGx] / 2.0, angle_resolution=2.5, layer=GC_hole_layer)
             # hole2 = pg.circle(radius = air_hole_diameter_list[iGx] / 2.0, angle_resolution=2.5,layer=0)
             # hole3 = pg.circle(radius = air_hole_diameter_list[iGx] / 2.0, angle_resolution=2.5,layer=0)
             hole1 = SWG_holes << hole
             hole2 = SWG_holes << hole
-            if iGy < (num_grating_periods_y - 1):
+            if iGy < (GCparams['num_grating_periods_y'] - 1):
                 hole3 = SWG_holes << hole
-                hole3.move(origin=[0, 0], destination=[grating_column_start_x + a_2DPhC_nm / 2.0,
-                                                       grating_column_start_y + a_2DPhC_nm / 2.0 * numpy.sqrt(3)])
+                hole3.move(origin=[0, 0], destination=[grating_column_start_x + GCparams['a_2DPhC'] / 2.0,
+                                                       grating_column_start_y + GCparams['a_2DPhC'] / 2.0 * numpy.sqrt(3)])
 
             hole1.move(origin=[0, 0], destination=[grating_column_start_x, grating_column_start_y])
-            hole2.move(origin=[0, 0], destination=[grating_column_start_x + a_2DPhC_nm, grating_column_start_y])
+            hole2.move(origin=[0, 0], destination=[grating_column_start_x + GCparams['a_2DPhC'], grating_column_start_y])
             # SWG_holes.add([hole1,hole2,hole3])
             # GC_holes_cell.add(gdspy.Round([grating_column_start_x, grating_column_start_y], air_hole_diameter_list[iGx] / 2.0, number_of_points=num_points))
             # GC_holes_cell.add(gdspy.Round([grating_column_start_x + a_2DPhC_nm, grating_column_start_y], air_hole_diameter_list[iGx] / 2.0, number_of_points=num_points))
@@ -58,85 +47,74 @@ def generate_grating_holes(air_hole_diameter_list_base,
             #     GC_holes_cell.add(gdspy.Round( [grating_column_start_x + a_2DPhC_nm / 2.0,grating_column_start_y + a_2DPhC_nm / 2.0 * numpy.sqrt(3)],air_hole_diameter_list[iGx] / 2.0, number_of_points=num_points))
     return SWG_holes
 
+
+def generate_silicon_skeleton(
+    GCparams
+):
+    test_PhC_bus_len = 14000
+    P1 = pp.straight(length=GCparams['grating_pad_length'])
+    P2 = pp.straight(length=test_PhC_bus_len)
+    pad_ref = P1.extrude(GCparams['grating_pad_width'], layer=2)
+    PhC_WG_ref = P2.extrude(GCparams['PhC_wy'], layer=2)
+
+    # define the taper
+    P3 = pp.straight(length=GCparams['taper_length'])
+    # Use the transitional CrossSection to create a Device
+    WG_trans = P3.extrude([GCparams['grating_pad_width'], GCparams['PhC_wy']], layer=2)
+
+    # add them all to a device together
+    silicon_skeleton = Device()
+    pad = silicon_skeleton << pad_ref
+    PhC_WG = silicon_skeleton << PhC_WG_ref
+    PhC_WG.movex(GCparams['taper_length'] + GCparams['grating_pad_length'])
+    wg3 = silicon_skeleton << WG_trans
+    wg3.movex(GCparams['grating_pad_length'])
+
+    silicon_skeleton_cutout_layer = 1
+
+    pad_and_taper = Device()
+    pad_and_taper.add_ref(WG_trans).movex(GCparams['grating_pad_length'])
+    pad_and_taper.add_ref(pad_ref)
+    # outline = pg.outline(pad_and_taper,distance=1000,precision=1e-9,max_points=8000,layer=silicon_skeleton_cutout_layer)
+    inversion = pg.invert(pad_and_taper, border=1000, precision=1e-9, layer=silicon_skeleton_cutout_layer)
+    skeleton_and_outline = Device()
+    skeleton_and_outline.add_ref(inversion)
+    skeleton_and_outline.add_ref(pad_and_taper)
+
+    outline_taper = pg.outline(wg3,distance=1000,precision=1e-9,layer=3)
+    skeleton_and_outline.add_ref(outline_taper)
+    silicon_cutout_only = pg.boolean(A=outline_taper,B=pad_and_taper,operation='not',precision=1e-9,num_divisions=[1,1],layer=4)
+    skeleton_and_outline.add_ref(silicon_cutout_only)
+    # skeleton_larger = pad_and_taper.copy(name='newcell',scale=1.1)
+    # skeleton_larger.remap_layers({2:3})
+    #get the outline itself that should remain
+
+    return [skeleton_and_outline,pad.center]
+
 def subwavelength_grating(
     air_hole_diameter_list_base,
     constgrating_airhole_scale_factor,
-    grating_period_x_start,
-    phase_factor,
-    grating_delta_index,
-    resonance_freq,
-    num_grating_periods_x,
-    num_grating_periods_y,
-    num_points,
-    a_2DPhC_nm,
-    grating_pad_length,
-    grating_pad_width,
-    grating_pad_buffer,
-    grating_pad_center_x,
-    grating_pad_center_y,
-    layer_num,
-    PhC_wy
+    GCparams,
+    grating_pad_center,
+    GC_hole_layer
 ):
     grating_holes = generate_grating_holes(air_hole_diameter_list_base,
     constgrating_airhole_scale_factor,
-    grating_period_x_start,
-    phase_factor,
-    grating_delta_index,
-    resonance_freq,
-    num_grating_periods_x,
-    num_grating_periods_y,
-    num_points,
-    a_2DPhC_nm,
-    grating_pad_length,
-    grating_pad_width,
-    grating_pad_buffer,
-    grating_pad_center_x,
-    grating_pad_center_y,
-    layer_num)
+    GCparams,
+    grating_pad_center,
+    GC_hole_layer)
 
-    #define taper
+    [silicon_stuff,pad_center] = generate_silicon_skeleton(GCparams)
 
-    # cross_section_width = CrossSection()
+    #align the silicon skeleton and holes
+    grating_holes.center=pad_center
+    # silicon_stuff.y = grating_holes.y
 
-    #define pad
+    #subtract the outline to get just the silicon remaining
+    grating_coupler=Device()
+    grating_coupler.add_ref(grating_holes)
+    grating_coupler.add_ref(silicon_stuff)
+    # grating_coupler.add_ref(skeleton_outline)
 
-
-    #define bit at the end
-
-    # X1 = CrossSection()
-    # X1.add(width=1.2, offset=0, layer=2, name='wg')
-    # X1.add(width=2.2, offset=0, layer=3, name='etch')
-    # X1.add(width=1.1, offset=3, layer=1, name='wg2')
-
-    # Create the second CrossSection that we want to transition to
-    # X2 = CrossSection()
-    # X2.add(width=1, offset=0, layer=2, name='wg')
-    # X2.add(width=3.5, offset=0, layer=3, name='etch')
-    # X2.add(width=3, offset=5, layer=1, name='wg2')
-
-    # To show the cross-sections, let's create two Paths and
-    # create Devices by extruding them
-    P1 = pp.straight(length=grating_pad_length)
-    P2 = pp.straight(length=14000)
-    WG1 = P1.extrude(grating_pad_width,layer=2)
-    WG2 = P2.extrude(PhC_wy,layer=2)
-
-    # Place both cross-section Devices and quickplot them
-
-    # Create the transitional CrossSection
-    # Xtrans = pp.transition(cross_section1=X1,
-    #                        cross_section2=X2,
-    #                        width_type='sine')
-    # # Create a Path for the transitional CrossSection to follow
-    P3 = pp.straight(length=180000)
-    # Use the transitional CrossSection to create a Device
-    WG_trans = P3.extrude([grating_pad_width,PhC_wy],layer=2)
-
-    silicon_skeleton = Device()
-    wg1 = silicon_skeleton << WG1
-    wg2 = silicon_skeleton << WG2
-    wg2.movex(180000)
-    wg3 = silicon_skeleton << WG_trans
-
-    return [grating_holes,silicon_skeleton]
+    return grating_coupler
 
--- photonics_lib_CAD.py
+++ photonics_lib_CAD.py
@@ -16,175 +16,61 @@ import phidl.geometry as pg
 from phidl import quickplot as qp
 
 if __name__ == "__main__":
-    # Examples
     lib = gdspy.GdsLibrary(unit=1e-09)
-
-    # Negative resist example
-    width = 0.45
-    bend_radius = 50.0
-    ring_radius = 20.0
-    taper_len = 50.0
-    input_gap = 150.0
-    io_gap = 500.0
-    wg_gap = 20.0
-    ring_gaps = [0.06 + 0.02 * i for i in range(8)]
-    GC_hole_radius = 0.153
-
-    ring = lib.new_cell("NRing")
-    ring.add(
-        gdspy.Round((ring_radius, 0), ring_radius, ring_radius - width, tolerance=0.001)
-    )
-
-    grat = lib.new_cell("NGrat")
-    grat.add(
-        grating(
-            0.626,
-            28,
-            0.5,
-            19,
-            (0, 0),
-            "+y",
-            1.55,
-            numpy.sin(numpy.pi * 8 / 180),
-            21.5,
-            width,
-            tolerance=0.001,
-        )
-    )
 
     #SWG_GC params
     #copied from old CAD code
-    param = {}
-    param['a_2DPhC'] = 232.61  # looks like this is the same as the
-    param['num_grating_periods_x'] = 20
-    param['num_grating_periods_y'] = 31  # SC total grating y width 12.6 um, maximum overlap with fiber
-    param['grating_period_x_start'] = 700
-    param['grating_pad_width'] = 12600  # assuming this is the same as the taperendwidth parameter in the lumerical geometry file
-    param['grating_first_index'] = 2.55
-    param['grating_delta_index'] = 0.02
-    param['phaseFactor'] = 0.582
-    param['a_2DPhC'] = 232.61  # looks like this is the same as the
-    param['grating_pad_length'] = 17000
-    param['grating_pad_buffer'] = (param['grating_pad_width'] - (param['num_grating_periods_y'] - 1) * param['a_2DPhC'] * numpy.sqrt(3)) / 2.0
-    param['grating_pad_offset'] = 2000
-    param['grating_pad_spacing'] = 3000
-    param['resonance']=1544
-    param['n_circle_points_GC'] = 110
-    param['PhC_wy'] = 600
-    grating_x_num = range(param['num_grating_periods_x'])  # range starts 0,1,2...num_grating_period_x-1
-    air_hole_diameter_list_base = [-0.09594 * param['a_2DPhC'] * (param['grating_first_index'] - param['grating_delta_index'] * x) ** 4 + 0.9546 * param['a_2DPhC'] * (param['grating_first_index'] - param['grating_delta_index'] * x) ** 3 - 3.586 * param['a_2DPhC'] * (
-                                               param['grating_first_index'] - param['grating_delta_index'] * x) ** 2 + 5.542 * param['a_2DPhC'] * (
-                                               param['grating_first_index'] - param['grating_delta_index'] * x) - 1.931 * param['a_2DPhC'] for x in grating_x_num]
+    GCparams = {}
+    GCparams['a_2DPhC'] = 232.61  # looks like this is the same as the
+    GCparams['num_grating_periods_x'] = 20
+    GCparams['num_grating_periods_y'] = 31  # SC total grating y width 12.6 um, maximum overlap with fiber
+    GCparams['grating_period_x_start'] = 700
+    GCparams['grating_pad_width'] = 12600  # assuming this is the same as the taperendwidth parameter in the lumerical geometry file
+    GCparams['grating_first_index'] = 2.55
+    GCparams['grating_delta_index'] = 0.02
+    GCparams['phaseFactor'] = 0.582
+    GCparams['a_2DPhC'] = 232.61  # looks like this is the same as the
+    GCparams['grating_pad_length'] = 17000
+    GCparams['grating_pad_buffer'] = (GCparams['grating_pad_width'] - (GCparams['num_grating_periods_y'] - 1) * GCparams['a_2DPhC'] * numpy.sqrt(3)) / 2.0
+    GCparams['grating_pad_offset'] = 2000
+    GCparams['grating_pad_spacing'] = 3000
+    GCparams['resonance']=1544
+    GCparams['n_circle_points_GC'] = 110
+    GCparams['PhC_wy'] = 600
+    GCparams['taper_length']=185000
+    grating_x_num = range(GCparams['num_grating_periods_x'])  # range starts 0,1,2...num_grating_period_x-1
+    air_hole_diameter_list_base = [-0.09594 * GCparams['a_2DPhC'] * (GCparams['grating_first_index'] - GCparams['grating_delta_index'] * x) ** 4 + 0.9546 * GCparams['a_2DPhC'] * (GCparams['grating_first_index'] - GCparams['grating_delta_index'] * x) ** 3 - 3.586 * GCparams['a_2DPhC'] * (
+            GCparams['grating_first_index'] - GCparams['grating_delta_index'] * x) ** 2 + 5.542 * GCparams['a_2DPhC'] * (
+                                           GCparams['grating_first_index'] - GCparams['grating_delta_index'] * x) - 1.931 * GCparams['a_2DPhC'] for x in grating_x_num]
 
-    # constgrating_airholescale = 1.01
-    # SW_GC_cell = lib.new_cell("N_SW_GC")
-    # [SW_GC,disregard] = subwavelength_grating(air_hole_diameter_list_base,
-    #     constgrating_airholescale,
-    #     param['grating_period_x_start'],
-    #     param['phaseFactor'],
-    #     param['grating_delta_index'],
-    #     param["resonance"],
-    #     param['num_grating_periods_x'],
-    #     param['num_grating_periods_y'],
-    #     param['n_circle_points_GC'],
-    #     param['a_2DPhC'],
-    #     param['grating_pad_length'],
-    #     param['grating_pad_width'],
-    #     param['grating_pad_buffer'],
-    #     0,
-    #     0,
-    #     SW_GC_cell)
+    #relevant params for sweep
 
-
-    # def subwavelength_grating(
-    #         air_hole_diameter_list_base,
-    #         constgrating_airhole_scale_factor,
-    #         grating_period_x_start,
-    #         phase_factor,
-    #         grating_delta_index,
-    #         resonance_freq,
-    #         num_grating_periods_x,
-    #         num_grating_periods_y,
-    #         num_points,
-    #         a_2DPhC_nm,
-    #         grating_pad_length,
-    #         grating_pad_width,
-    #         grating_pad_buffer,
-    #         grating_pad_center_x,
-    #         grating_pad_center_y,
-    #         GC_holes_cell,
-    # ):
-
-    taper = lib.new_cell("NTaper")
-    taper.add(gdspy.Path(0.12, (0, 0)).segment(taper_len, "+y", final_width=width))
-
-    circle_array = lib.new_cell("N_GC_holes")
-
-    circle_array.add(gdspy.Round((GC_hole_radius, 0), GC_hole_radius, tolerance=0.001))
-
-    c = lib.new_cell("Negative")
-    for i, gap in enumerate(ring_gaps):
-        path = gdspy.FlexPath(
-            [(input_gap * i, taper_len)],
-            width=width,
-            corners="circular bend",
-            bend_radius=bend_radius,
-            gdsii_path=True,
-        )
-        path.segment((0, 600 - wg_gap * i), relative=True)
-        path.segment((io_gap, 0), relative=True)
-        path.segment((0, 300 + wg_gap * i), relative=True)
-        c.add(path)
-        c.add(gdspy.CellReference(ring, (input_gap * i + width / 2 + gap, 300)))
-    c.add(gdspy.CellArray(taper, len(ring_gaps), 1, (input_gap, 0), (0, 0)))
-    c.add(
-        gdspy.CellArray(
-            grat, len(ring_gaps), 1, (input_gap, 0), (io_gap, 900 + taper_len)
-        )
-    )
-
-    c.add(gdspy.CellArray(circle_array, len(ring_gaps), 1, (input_gap, 0), (0, 0)))
-    # c.add(SW_GC)
-
-    # Save to a gds file and check out the output
-    lib.write_gds("photonics_1layersaved.gds",cells=[c])
+    xspacing = 135000
+    yspacing = 250000
 
     GC_scales = [0.97,1.01,1.05]
     grating_coupler_list = []
-    silicon_skeleton_list = []
+    # silicon_skeleton_list = []
+    grating_pad_center = [0,0]
+    GC_hole_layer = 0
     for GC_scale in GC_scales:
-        [device_format_holes,silicon_skeleton] = subwavelength_grating(air_hole_diameter_list_base,
-        GC_scale,
-        param['grating_period_x_start'],
-        param['phaseFactor'],
-        param['grating_delta_index'],
-        param["resonance"],
-        param['num_grating_periods_x'],
-        param['num_grating_periods_y'],
-        param['n_circle_points_GC'],
-        param['a_2DPhC'],
-        param['grating_pad_length'],
-        param['grating_pad_width'],
-        param['grating_pad_buffer'],
-        0,
-        0,
-        0,
-        param['PhC_wy'])
-        grating_coupler_list.append(device_format_holes)
-        silicon_skeleton_list.append(silicon_skeleton)
-    grid_of_holes = pg.grid(grating_coupler_list, spacing=(5,1),separation=True,shape=(1,3),align_x='x',align_y='y',edge_x='x',edge_y='ymax')
-    grid_of_silicon = pg.grid(silicon_skeleton_list, spacing=(5,1),separation=True,shape=(1,3),align_x='x',align_y='y',edge_x='x',edge_y='ymax')
+        grating_coupler = subwavelength_grating(air_hole_diameter_list_base,GC_scale,GCparams,grating_pad_center,GC_hole_layer)
+        grating_coupler_list.append(grating_coupler)
+        # silicon_skeleton_list.append(silicon_skeleton)
+    # grid_of_holes = pg.grid(grating_coupler_list, spacing=(xspacing,yspacing),separation=True,shape=(1,3),align_x='x',align_y='y',edge_x='x',edge_y='ymax')
+    # grid_of_silicon = pg.grid(silicon_skeleton_list, spacing=(xspacing,yspacing),separation=True,shape=(1,3),align_x='x',align_y='y',edge_x='x',edge_y='ymax')
+    grid_of_GCs = pg.grid(grating_coupler_list,spacing=(xspacing,yspacing),separation=True,shape=(1,3),align_x='x',align_y='y',edge_x='x',edge_y='ymax')
 
     # grid_of_holes_2 = pg.grid(grating_coupler_list, spacing=(5,1),separation=True,shape=(1,3),align_x='x',align_y='y',edge_x='x',edge_y='ymax')
-    big_boy = Device()
-    grid1 = big_boy << grid_of_holes
-    grid2 = big_boy << grid_of_holes
-    silicon_structure1 = big_boy << grid_of_silicon
-    silicon_structure2 = big_boy << grid_of_silicon
-    bounding_box_grid1 = grid1.get_bounding_box()
-    silicon_structure_box_grid1 = silicon_structure1.get_bounding_box()
-    grid2.move(origin=bounding_box_grid1[0],destination=bounding_box_grid1[1])
-    silicon_structure2.move(origin=silicon_structure_box_grid1[0],destination=silicon_structure_box_grid1[1])
+    # big_boy = Device()
+    # grid1 = big_boy << grid_of_holes
+    # grid2 = big_boy << grid_of_holes
+    # silicon_structure1 = big_boy << grid_of_silicon
+    # silicon_structure2 = big_boy << grid_of_silicon
+    # bounding_box_grid1 = grid1.get_bounding_box()
+    # silicon_structure_box_grid1 = silicon_structure1.get_bounding_box()
+    # grid2.move(origin=bounding_box_grid1[0],destination=bounding_box_grid1[1])
+    # silicon_structure2.move(origin=silicon_structure_box_grid1[0],destination=silicon_structure_box_grid1[1])
 
     # big_boy.remap_layers({0:1})
     # D = pg.gridsweep(
@@ -203,6 +89,6 @@ if __name__ == "__main__":
     #
     # # qp(D)
     # D.write_gds('phidlcheck.gds')
-    grid_of_holes.write_gds('gridsweepcheck.gds',unit=1e-9,precision=1e-12)
-    big_boy.write_gds('checking_deviceofdevice.gds',unit=1e-9,precision=1e-12)
+    # grid_of_holes.write_gds('gridsweepcheck.gds',unit=1e-9,precision=1e-12)
+    grid_of_GCs.write_gds('checking_deviceofdevice.gds',unit=1e-9,precision=1e-12)
     # gdspy.LayoutViewer(lib)
